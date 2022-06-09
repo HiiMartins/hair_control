9f6dc1d3b0064519a167e0ad2cdffceab6fa690b
HiiMartins <higor.dsm10@gmail.com>
2022-06-09 11:46:11 -0300

    FEAT: implemented changeset/2

--- lib/hair_control/client.ex
+++ lib/hair_control/client.ex
@@ -24,10 +24,11 @@ defmodule HairControl.Client do
     |> apply_action(:insert)
   end
 
+  def changeset(client, params), do: create_changeset(client, params)
   defp changeset(params), do: create_changeset(%__MODULE__{}, params)
 
-  defp create_changeset(module, params) do
-    module
+  defp create_changeset(module_or_client, params) do
+    module_or_client
     |> cast(params, @params)
     |> validate_required(@required_params)
   end
