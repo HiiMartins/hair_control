b2585961917cd11eac5ceb840f0f4628c6f9bafd
HiiMartins <higor.dsm10@gmail.com>
2022-06-18 19:37:14 -0300

    FEAT: added delete lofic to servicee

---
+++ lib/hair_control/servicee/delete.ex
@@ -0,0 +1,18 @@
+defmodule HairControl.Service.Delete do
+  alias HairControl.{Service, Repo}
+  alias Ecto.UUID
+
+  def call(%{"id" => uuid}) do
+    case UUID.cast(uuid) do
+      :error -> {:error, "Invalid ID format!"}
+      {:ok, uuid} -> fetch_service(uuid)
+    end
+  end
+
+  defp fetch_service(uuid) do
+    case Repo.get(Service, uuid) do
+      nil -> {:error, "Client not Found!"}
+      service -> Repo.delete(service)
+    end
+  end
+end
