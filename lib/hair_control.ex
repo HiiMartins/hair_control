428a7f51dbf31209f1890c34d3ce6f262e54536d
HiiMartins <higor.dsm10@gmail.com>
2022-06-20 00:20:42 -0300

    CHORE: added all facades to service module

--- lib/hair_control.ex
+++ lib/hair_control.ex
@@ -6,13 +6,19 @@ defmodule HairControl do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
-  alias HairControl.{Employee, Client}
+  alias HairControl.{Employee, Client, Service}
 
   defdelegate create_employee(params), to: Employee.Create, as: :call
 
   defdelegate create_client(params), to: Client.Create, as: :call
   defdelegate delete_client(params), to: Client.Delete, as: :call
   defdelegate fetch_client(params), to: Client.Get, as: :call
-  defdelegate list_client(), to: Client.List, as: :call
+  defdelegate list_clients(), to: Client.List, as: :call
   defdelegate update_client(params), to: Client.Update, as: :call
+
+  defdelegate create_service(params), to: Service.Create, as: :call
+  defdelegate delete_service(params), to: Service.Delete, as: :call
+  defdelegate fetch_service(params), to: Service.Get, as: :call
+  defdelegate list_services(), to: Service.List, as: :call
+  defdelegate update_service(params), to: Service.Update, as: :call
 end
