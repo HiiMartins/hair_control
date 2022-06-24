79216f31f9ddbb5d8aaa5ebba493d478af04f3b1
HiiMartins <higor.dsm10@gmail.com>
2022-06-24 17:03:22 -0300

    FEAT: added schema and build changeset to sale module

---
+++ lib/hair_control/sale.ex
@@ -0,0 +1,36 @@
+defmodule HairControl.Sale do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, Ecto.UUID, autogenerate: true}
+  @foreign_key_type Ecto.UUID
+
+  schema "sales" do
+    field :payment_method, Ecto.Enum, values: [:dinheiro, :cartão, :fiado]
+
+    belongs_to :employee, HairControl.Employee
+    belongs_to :client, HairControl.Client
+    belongs_to :service, HairControl.Service
+
+    timestamps()
+  end
+
+  @required_params [:payment_method, :employee_id, :client_id, :service_id]
+
+  def build(params) do
+    params
+    |> changeset()
+    |> apply_action(:insert)
+  end
+
+  defp changeset(params), do: create_changeset(params)
+
+  defp create_changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> assoc_constraint(:employee)
+    |> assoc_constraint(:service)
+    |> assoc_constraint(:client)
+  end
+end
