bbb789120e98612897afe2e45b09fc8585c43324
HiiMartins <higor.dsm10@gmail.com>
2022-06-29 10:04:13 -0300

    CHORE: added logic to constraint validation

--- lib/hair_control/sale/create.ex
+++ lib/hair_control/sale/create.ex
@@ -1,11 +1,46 @@
 defmodule HairControl.Sale.Create do
-  alias HairControl.{Sale, Repo}
+  alias HairControl.{Client, Employee, Sale, Service, Repo}
 
   def call(params) do
     params
     |> Sale.build()
+    |> fetch_constraint()
     |> create_sale()
   end
+
+  defp fetch_constraint(params) do
+    params
+    |> fetch_employee()
+    |> fetch_client()
+    |> fetch_service()
+  end
+
+  defp fetch_employee({:ok, %Sale{employee_id: employee_id}} = params) do
+    case Repo.get(Employee, employee_id) do
+      nil -> {:error, "Employee not found!"}
+      _result -> {:ok, params}
+    end
+  end
+
+  defp fetch_employee({:error, _reason} = error), do: error
+
+  defp fetch_client({:ok, %Sale{client_id: client_id}} = params) do
+    case Repo.get(Client, client_id) do
+      nil -> {:error, "Client not found!"}
+      _result -> {:ok, params}
+    end
+  end
+
+  defp fetch_client({:error, _reason} = error), do: error
+
+  defp fetch_service({:ok, %Sale{service_id: service_id}} = params) do
+    case Repo.get(Service, service_id) do
+      nil -> {:error, "Service not found!"}
+      _result -> {:ok, params}
+    end
+  end
+
+  defp fetch_service({:error, _reason} = error), do: error
 
   defp create_sale({:ok, struct}), do: Repo.insert(struct)
   defp create_sale({:error, _changeset} = error), do: error
