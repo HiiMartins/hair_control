aac6334cab05535301f8c946f3189d9821fcd2e1
HiiMartins <higor.dsm10@gmail.com>
2022-07-05 13:31:50 -0300

    CHORE: implemented pattern matching in delete/2 parameters

--- lib/hair_control_web/controllers/services_controller.ex
+++ lib/hair_control_web/controllers/services_controller.ex
@@ -9,8 +9,8 @@ defmodule HairControlWeb.ServicesController do
     |> handle_response(conn, "create.json", :created)
   end
 
-  def delete(conn, params) do
-    params
+  def delete(conn, %{"id" => id}) do
+    id
     |> HairControl.delete_service()
     |> handle_response(conn, "delete.json", :ok)
   end
