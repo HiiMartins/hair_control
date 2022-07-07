a3481d786eab533f1e8e785295007e8c79e97140
HiiMartins <higor.dsm10@gmail.com>
2022-07-06 23:00:48 -0300

    CHORE: added the views and controller in sale endpoint

--- lib/hair_control_web/controllers/sales_controller.ex
+++ lib/hair_control_web/controllers/sales_controller.ex
@@ -9,6 +9,23 @@ defmodule HairControlWeb.SalesController do
     |> handle_response(conn, "create.json", :created)
   end
 
+  def delete(conn, %{"id" => id}) do
+    id
+    |> HairControl.delete_sale()
+    |> handle_response(conn, "delete.json", :ok)
+  end
+
+  def list(conn, _params) do
+    HairControl.list_sales()
+    |> handle_response(conn, "list.json", :ok)
+  end
+
+  def show(conn, %{"id" => id}) do
+    id
+    |> HairControl.fetch_sale()
+    |> handle_response(conn, "show.json", :ok)
+  end
+
   defp handle_response({:ok, sale}, conn, view, status) do
     conn
     |> put_status(status)
--- lib/hair_control_web/views/sales_view.ex
+++ lib/hair_control_web/views/sales_view.ex
@@ -4,23 +4,55 @@ defmodule HairControlWeb.SalesView do
   alias HairControl.Sale
 
   def render("create.json", %{
-    sale: %Sale{
-      id: id,
-      employee_id: employee_id,
-      service_id: service_id,
-      client_id: client_id,
-      inserted_at: inserterd_at
-    }
-  }) do
+        sale: %Sale{
+          id: id,
+          payment_method: payment_method,
+          employee_id: employee_id,
+          service_id: service_id,
+          client_id: client_id,
+          inserted_at: inserterd_at
+        }
+      }) do
     %{
       messagge: "Sale created!",
       sale: %{
         id: id,
+        payment_method: payment_method,
         employee_id: employee_id,
         client_id: client_id,
         service_id: service_id,
         inserterd_at: inserterd_at
       }
+    }
+  end
+
+  def render("delete.json", %{sale: %Sale{id: id}}) do
+    %{
+      message: "The sale de id #{id} was deleted with sucess!"
+    }
+  end
+
+  def render("list.json", %{sale: sale}) do
+    Enum.map(sale, &render("show.json", %{client: &1}))
+  end
+
+  def render("show.json", %{
+        sale: %Sale{
+          id: id,
+          payment_method: payment_method,
+          client_id: client_id,
+          employee_id: employee_id,
+          service_id: service_id,
+          inserted_at: inserted_at
+        }
+      }) do
+    %{
+      id: id,
+      payment_method: payment_method,
+      client_id: client_id,
+      employee_id: employee_id,
+      service_id: service_id,
+      inserted_at: inserted_at
     }
   end
 end
