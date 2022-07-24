dc10c8a53fd84c09bb4ab720a45333bb3e9b0914
HiiMartins <higor.dsm10@gmail.com>
2022-07-24 17:16:11 -0300

    FEAT: added logic to insert payroll register

--- lib/hair_control/employee/payroll/create.ex
+++ lib/hair_control/employee/payroll/create.ex
@@ -1,11 +1,12 @@
 defmodule HairControl.Employee.Payroll.Create do
   alias HairControl.{Employee, Repo}
-  alias Employee.Payroll
+  alias Employee.{Payroll, Update}
 
   def call(params) do
     params
     |> Payroll.build()
     |> fetch_constraint()
+    |> preloud_assotions()
     |> create_payroll()
   end
 
@@ -17,6 +18,14 @@ defmodule HairControl.Employee.Payroll.Create do
   end
 
   defp fetch_constraint({:error, _changeset} = error), do: error
+
+  defp preloud_assotions({:ok, struct}) do
+    struct
+    |> Repo.preload([:employee])
+    |> Update.update_total_received()
+  end
+
+  defp preloud_assotions({:error, _reason} = error), do: error
 
   defp create_payroll({:ok, struct}), do: Repo.insert(struct)
   defp create_payroll({:error, _changeset} = error), do: error
