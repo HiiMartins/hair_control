8dfac064163b9e8a2f8b4ee9aeadfdd246b32cfd
HiiMartins <higor.dsm10@gmail.com>
2022-10-04 17:04:32 -0300

    CHORE: refactored the filtering defs

--- lib/hair_control/sale/list.ex
+++ lib/hair_control/sale/list.ex
@@ -1,7 +1,7 @@
 defmodule HairControl.Sale.List do
   import Ecto.Query
 
-  alias HairControl.{Client, Employee, Sale, Service, Repo}
+  alias HairControl.{Repo, Sale}
 
   # def call(%{}), do: {:ok, Repo.all(from(s in Sale, preload: [:client, :employee, :service]))}
 
@@ -23,13 +23,22 @@ defmodule HairControl.Sale.List do
         query |> where([q], q.payment_method == ^payment_method)
 
       {"service_name", service_name}, query ->
-        from(q in query, join: s in Service, where: s.title == ^service_name)
+        from q in query,
+          join: s in assoc(q, :service),
+          where: s.title == ^service_name,
+          preload: [service: s]
 
       {"employee_name", employee_name}, query ->
-        from(q in query, join: e in Employee, where: e.name == ^employee_name)
+        from q in query,
+          join: e in assoc(q, :employee),
+          where: e.name == ^employee_name,
+          preload: [employee: e]
 
       {"client_name", client_name}, query ->
-        from(q in query, join: c in Client, where: c.name == ^client_name)
+        from q in query,
+          join: c in assoc(q, :client),
+          where: c.name == ^client_name,
+          preload: [client: c]
 
       {"data_from", data_from}, query ->
         query |> where([q], q.inserted_at >= ^data_from)
